abd70ca1b2bf41eb4b61218f350b4124a6ab563a
Jonathan Atsu <jona.atsu@gmail.com>
2025-04-01 10:30:46 +0200

    make sure that user infos like name and email are in id_token

--- frontend/restaurateurfront/restaurateur/src/main.tsx
+++ frontend/restaurateurfront/restaurateur/src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { WebStorageStateStore } from "oidc-client-ts";
-import { AuthProvider } from "react-oidc-context"
+import { AuthProvider } from "react-oidc-context";
 
 const oidcConfig = {
   authority: "https://instance1-el5q1i.zitadel.cloud/",
   client_id: "312751992336403117",
   redirect_uri: "http://localhost:5174/restaurateur/",
   userStore: new WebStorageStateStore({ store: window.localStorage }),
+  scope: "openid profile email", // Ensure needed scopes are requested
+  response_type: "code",
+  loadUserInfo: true, // Important to fetch additional claims
   // ...
 };
 
