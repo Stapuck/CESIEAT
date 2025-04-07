9dd1fc6eca0cff2fb294b1a61810fd02ece9e42f
Nathan Lorit <nathan.lorit@viacesi.fr>
2025-04-07 14:39:27 +0200

    clean up

--- frontend/technicalfront/technical/src/App.tsx
+++ frontend/technicalfront/technical/src/App.tsx
@@ -4,14 +4,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import ErrorBoundary from "./components/ErrorBoundary";
-import ApiDocumentationPage from "./pages/ApiDocumentationPage";
-import ApiKeysPage from "./pages/ApiKeysPage";
-import ApiUsagePage from "./pages/ApiUsagePage";
+
 import EditProfilePage from "./pages/EditProfilePage";
-import MyDownloadsPage from "./pages/MyDownloadsPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
+
 import ProfilePage from "./pages/ProfilePage";
-import SettingsPage from "./pages/SettingsPage";
 import Protected from "./pages/Protected";
 import LoginPage from "./pages/LoginPage"; // Importez la page de connexion
 import CreateProductPage from "./pages/CreateProductPage";
@@ -24,7 +20,8 @@ function App() {
         <div className="container mx-auto p-2 h-full">
           <Routes>
             <Route path="/technical/" element={<HomePage />} />
-            <Route path="/technical/login" element={<LoginPage />} /> {/* Nouvelle route */}
+            <Route path="/technical/login" element={<LoginPage />} />{" "}
+            {/* Nouvelle route */}
             <Route element={<Protected />}>
               <Route index element={<HomePage />} />
               {/* Routes protégées */}
@@ -36,40 +33,11 @@ function App() {
                 path="/technical/profile/edit"
                 element={<EditProfilePage />}
               ></Route>
-              <Route
-                path="/technical/profile/settings"
-                element={<SettingsPage />}
-              ></Route>
 
-              {/* Routes d'API et d'intégration */}
-              <Route
-                path="/technical/api-keys"
-                element={<ApiKeysPage />}
-              ></Route>
-              <Route
-                path="/technical/api-documentation"
-                element={<ApiDocumentationPage />}
-              ></Route>
-              <Route
-                path="/technical/api-usage"
-                element={<ApiUsagePage />}
-              ></Route>
-
-              {/* Routes de gestion des composants */}
-
-              <Route
-                path="/technical/my-downloads"
-                element={<MyDownloadsPage />}
-              ></Route>
-              <Route
-                path="/technical/product/:id"
-                element={<ProductDetailPage />}
-              ></Route>
               <Route
                 path="/technical/create-product"
                 element={<CreateProductPage />}
               ></Route>
-
 
               {/* Routes de gestion des containers */}
             </Route>
