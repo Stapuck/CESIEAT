5ac55cd52f0c0f96c7b735b5a8e44dfb88d0dbc3
Jonathan Atsu <jona.atsu@gmail.com>
2025-04-07 09:43:21 +0200

    first sidebar

--- frontend/commercialfront/commercial/src/App.tsx
+++ frontend/commercialfront/commercial/src/App.tsx
@@ -1,28 +1,33 @@
-// import { Route, Routes } from "react-router-dom"
-// import Protected from "./pages/Protected"
-// import LoginButton from "./components/LoginButton"
+import { Route, Routes } from "react-router-dom"
+import Protected from "./pages/Protected"
+import LoginButton from "./components/LoginButton"
+import Sidebar from "./pages/Sidebar"
+import Home from "./pages/Home"
 
-// function App() {
-//   return (
-//         <Routes>
-//           <Route path="/commercial/tmplogin" element={<LoginButton />} />
-//           <Route element={<Protected />}>
-//           </Route>
-//         </Routes>
-//   )
-// }
-
-// export default App
-
-
-import { Button } from "@/components/ui/button"
- 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me </Button>
-    </div>
+        <Routes>
+          <Route element={<Sidebar />}>
+          <Route path="/commercial/tmplogin" element={<LoginButton />} />
+          <Route path="/commercial/" element={<Home />} />
+          </Route>
+          <Route element={<Protected />}>
+          </Route>
+        </Routes>
   )
 }
- 
+
 export default App
+
+
+// import { Button } from "@/components/ui/button"
+ 
+// function App() {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-svh">
+//       <Button>Click me </Button>
+//     </div>
+//   )
+// }
+ 
+// export default App
---
+++ frontend/commercialfront/commercial/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>Dashboard
+      <div>ok</div>
+    </div>
+  )
+}
+
+export default Home
