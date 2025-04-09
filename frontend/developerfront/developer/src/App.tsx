679f820c677e4b9466179ab140c9f63fdea0a7d6
Nathan Lorit <nathan.lorit@viacesi.fr>
2025-04-09 09:16:17 +0200

    fix developer

--- frontend/developerfront/developer/src/App.tsx
+++ frontend/developerfront/developer/src/App.tsx
@@ -1,39 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-            <Route path="developer/aboutus" element={<AboutUs />} />
-            <Route path="developer/faq" element={<FAQPage />} />
-            <Route path="developer/contact" element={<Contact />} />
-            <Route
-              path="developer/confidentialité"
-              element={<ConfidentialityPage />}
-            />
-            <Route path="developer/terms" element={<Terms />} />
-            <Route path="developer/tarif" element={<TarifsPage />} />
-            <Route path="developer/promotion" element={<Promotion />} />
-        <ScrollToTop/>
 
-            <Route path="developer/aboutus" element={<AboutUs />} />
-            <Route path="developer/faq" element={<FAQPage />} />
-            <Route path="developer/contact" element={<Contact />} />
-            <Route
-              path="developer/confidentialité"
-              element={<ConfidentialityPage />}
-            />
-            <Route path="developer/terms" element={<Terms />} />
-            <Route path="developer/tarif" element={<TarifsPage />} />
-            <Route path="developer/promotion" element={<Promotion />} />
 import HomePage from "./pages/HomePage";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ApiKeysPage from "./pages/ApiKeysPage";
-import EditProfilePage from "./pages/EditProfilePage";
-import MyDownloadsPage from "./pages/MyDownloadsPage";
+
 import ProfilePage from "./pages/ProfilePage";
-import SettingsPage from "./pages/SettingsPage";
-import ApiDocumentationPage from "./pages/ApiDocumentationPage";
-import ApiUsagePage from "./pages/ApiUsagePage";
-import CreateProductPage from "./pages/CreateProductPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
+
 import Protected from "./pages/Protected";
 import LoginButton from "./components/LoginButton";
 import ScrollToTop from "./components/ScrollToTop";
@@ -69,39 +42,9 @@ function App() {
 
             {/* Routes de gestion du compte */}
             <Route path="/developer/profile" element={<ProfilePage />}></Route>
-            <Route
-              path="/developer/profile/edit"
-              element={<EditProfilePage />}
-            ></Route>
-            <Route
-              path="/developer/profile/settings"
-              element={<SettingsPage />}
-            ></Route>
 
-            {/* Routes d'API et d'intégration */}
-            <Route path="/developer/api-keys" element={<ApiKeysPage />}></Route>
-            <Route
-              path="/developer/api-documentation"
-              element={<ApiDocumentationPage />}
-            ></Route>
-            <Route
-              path="/developer/api-usage"
-              element={<ApiUsagePage />}
-            ></Route>
 
-            {/* Routes de gestion des composants */}
-            <Route
-              path="/developer/create-product"
-              element={<CreateProductPage />}
-            ></Route>
-            <Route
-              path="/developer/my-downloads"
-              element={<MyDownloadsPage />}
-            ></Route>
-            <Route
-              path="/developer/product/:id"
-              element={<ProductDetailPage />}
-            ></Route>
+
           </Route>
         </Routes>
       </div>
